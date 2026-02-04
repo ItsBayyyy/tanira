52839f2b1e63fb01d838e4a39b39c19522105a29
ItsBayy <petikang07@gmail.com>
2026-02-05 01:02:15 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // --- KONFIGURASI SESSION ---
 app.use(session({
